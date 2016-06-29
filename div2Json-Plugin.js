c9462a93679474c4d9752022a7fefa8385e7fb7b
tj <tarang.lute@gmail.com>
2016-06-29 13:38:06 +0530

    Updated some methods
    Updated some methods

--- div2Json-Plugin.js
+++ div2Json-Plugin.js
@@ -1,130 +1,68 @@
 /*
-	Author : Tarang Lute
-	Plug-in Name : Div2Json
-	Description:Plug-in to generate json from elements present inside div.
-*/
-(function ( $ ) {
-	$.fn.div2Json=function(){
-		var elements = [];
-		var jsonData={};
-		var inDiv=this;
-
-		elements.push({
-			'control': 'input[type=checkbox]'
-		});
-		
-		elements.push({
-        'control': 'input[type=radio]'
-		});
-		
-		elements.push({
-			'control': 'input[type=text]'
-		});
-		elements.push({
-			'control': 'option:selected'
-		});
-		
-		elements.push({
-			'control': 'textarea'
-		});
-		
-		// Parse for each element
-		$.each(elements, function (index) {
-			$(inDiv).find(elements[index]['control']).each(function () {
-				   if (typeof (level) !== 'undefined') {
-					var controlItem = $(this).get(0);
-					if (typeof (controlItem) !== 'undefined') {
-						var tgname = $(this).get(0).tagName;
-						if (typeof (tgname) !== 'undefined') {
-							controlValue=GetControlValue(this,tgname);
-							if (typeof (controlValue) !== 'undefined' && controlValue!=null ) {
-								HasProperty(emptySchmea, level, controlValue);
-							}
-						}
-					}
-				}
-			});
-		});
-		return jsonData;
-	};
-}( jQuery ));
-
-function GetControlValue(control,tgname) {
-     var controlValue = '';
-    if (tgname === 'OPTION') {
-        controlValue = $(control).text();
-    }else if(tgname=='TEXTAREA'){
-        controlValue = $(control).val();
-    }else {
-        var tgname = $(control).get(0).type;
-        if (tgname === 'text') {
-            controlValue = $(control).val();
-        } else {
-            if ($(control).is(":checked")) {
-                if (tgname == 'radio') {
-                    controlValue = $(control).val();
-                } else {
-                    controlValue = $(control).attr("id");
+            Author : Tarang Lute
+            Plug-in Name : Div2Json
+            Description:This plug-in generate json data from div elements.Key Duplication not supported.
+            Currently supports controls like text-box,check-box,select. 
+        */
+        (function ($) {
+            $.fn.div2Json = function () {
+                var elements = [];
+                var jsonData = {};
+                var inDiv = this;
+        
+                elements.push({
+                    'control': 'input[type=radio]',
+                    'value': 'value'
+                });
+                
+                elements.push({
+                    'control': 'input[type=hidden]',
+                    'value': 'value'
+                });
+                
+                elements.push({
+                    'control': 'input[type=checkbox]',
+                    'value': 'checked'
+                });
+                elements.push({
+                    'control': 'input[type=text]',
+                    'value': 'value'
+                });
+                elements.push({
+                    'control': 'option:selected',
+                    'value': 'value'
+                });
+        
+                // Parse for each element
+                $.each(elements, function (index) {
+                    $(inDiv).find(elements[index]['control']).each(function () {
+                        var level;
+                        if (typeof (this) == 'select') {
+                            level = $(this).parent().attr("datalevel");
+                        } else {
+                            level = $(this).attr("datalevel");
+                        }
+                        if (typeof (level) !== 'undefined') {
+                            HasProperty(jsonData, level, this[elements[index]['value']]);
+                        }
+                    });
+                });
+                return jsonData;
+            };
+        } (jQuery));
+        function HasProperty(obj, nestlevel, value) {
+            var parts = nestlevel.split('-');
+            for (var i = 0, l = parts.length; i < l; i++) {
+                var part = parts[i];
+                if (obj !== null && typeof obj === "object" && part in obj) {
+                    obj = obj[part];
                 }
-            }else{
-                controlValue=null;
-            }
-        }
-    }
-    return controlValue;
-}
-
-function HasProperty(obj, prop, value) {
-    var parts = prop.split('-');
-    for (var i = 0, l = parts.length; i < l; i++) {
-        var containsString = parts[i].indexOf("|");
-        var part = '';
-        var datatype = '';
-        if (containsString < 0) {
-            part = parts[i];
-        } else {
-            var prt = parts[i].split('|');
-            part = prt[0];
-            datatype = prt[1];
-        }
-
-        if (obj !== null && typeof obj === "object" && part in obj) {
-            obj = obj[part];
-        } else {
-            var itemindex;
-            if (datatype == '') {
-                itemindex = parts.indexOf(part) + 1;
-            } else {
-                itemindex = parts.indexOf(part + "|" + datatype) + 1;
-            }
-
-            var node = {};
-            if (itemindex === parts.length) {
-                node = GetTypeCastValue(datatype, value);
-            }
-            obj[part] = node;
-            obj = obj[part];
-        }
-    }
-}
-
-function GetTypeCastValue(datatype, value) {
-    var typevalue = value;
-    if (datatype != '') {
-        switch (datatype.toLowerCase()) {
-            case "string":
-                typevalue = String(value);
-                break;
-            case "int":
-                var result = parseInt(value);
-                if (!isNaN(result)) {
-                    typevalue = result;
+                else {
+                    var itemindex = parts.indexOf(part) + 1;
+                    var node = {};
+                    if (itemindex === parts.length) {
+                        node = value;
+                    }
+                    obj[part] = node;
+                    obj = obj[part];
                 }
-                break;
-            case "bool":
-                typevalue = Boolean(value);
-                break;
-        }
-    }
-    return typevalue;
-}
